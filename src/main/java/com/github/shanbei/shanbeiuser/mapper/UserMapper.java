f211fc61b588f64951b84010fa6bf73a8d5901bf
zhengqingquan <55186872+zhengqingquan@users.noreply.github.com>
2023-02-24 00:11:18 +0800

    创建数据库，用Mybatis连接。但数据库的主键自增字段不是从1开始的。

--- src/main/java/com/github/shanbei/shanbeiuser/mapper/UserMapper.java
+++ src/main/java/com/github/shanbei/shanbeiuser/mapper/UserMapper.java
@@ -1,8 +1,18 @@
 package com.github.shanbei.shanbeiuser.mapper;
 
+import com.github.shanbei.shanbeiuser.model.domain.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.github.shanbei.shanbeiuser.model.User;
 
+/**
+* @author 96400
+* @description 针对表【user(用户表)】的数据库操作Mapper
+* @createDate 2023-02-23 23:02:11
+* @Entity generator.domain.User
+*/
 public interface UserMapper extends BaseMapper<User> {
 
 }
+
+
+
+
