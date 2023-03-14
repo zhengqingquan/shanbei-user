6933fa4ae1e391ddecb7d0aa31487b419217b4e1
zhengqingquan <964005301@qq.com>
2023-03-14 10:16:14 +0800

    修改业务异常类。

--- src/main/java/com/github/shanbei/shanbeiuser/exception/BusinessException.java
+++ src/main/java/com/github/shanbei/shanbeiuser/exception/BusinessException.java
@@ -22,21 +22,17 @@ public class BusinessException extends RuntimeException {
      */
     private final String description;
 
-    public BusinessException(String message, int code, String description) {
+    public BusinessException(int code, String message, String description) {
         super(message);
         this.code = code;
         this.description = description;
     }
 
     public BusinessException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.code = errorCode.getCode();
-        this.description = errorCode.getDescription();
+        this(errorCode.getCode(), errorCode.getMessage(), errorCode.getDescription());
     }
 
     public BusinessException(ErrorCode errorCode, String description) {
-        super(errorCode.getMessage());
-        this.code = errorCode.getCode();
-        this.description = description;
+        this(errorCode.getCode(), errorCode.getMessage(), description);
     }
 }
