3c02f49632cdf7cb97923c132a0299fa669c8bc9
zhengqingquan <964005301@qq.com>
2023-03-17 11:25:40 +0800

    修改一些细节

--- src/main/java/com/github/shanbei/shanbeiuser/common/HttpStatus.java
+++ src/main/java/com/github/shanbei/shanbeiuser/common/HttpStatus.java
@@ -2,11 +2,10 @@ package com.github.shanbei.shanbeiuser.common;
 
 /**
  * 返回状态码
- * 
+ *
  * @author zhengqingquan
  */
-public class HttpStatus
-{
+public class HttpStatus {
     /**
      * 操作成功
      */
